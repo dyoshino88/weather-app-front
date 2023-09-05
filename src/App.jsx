cdf97f7ff5a10b1f58cff1c9a53a595bcf55cb9f
dyoshino88 <yoshino0707dh@gmail.com>
2023-09-05 10:15:01 +0900

    wetherchoice

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,41 @@
-import React from "react";
-import axios from "axios";
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
-    const [data, setData] = React.useState();
-    const url = "https://young-mesa-75839-8d20ed811f40.herokuapp.com/";
+  const [cityName, setCityName] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
 
-    const GetData = () => {
-        axios.get(url).then((res) => {
-            setData(res.data);
-        });
-    };
-    return (
+  const handleGetWeather = async () => {
+    try {
+      const response = await axios.get(`https://young-mesa-75839-8d20ed811f40.herokuapp.com/get_weather/${cityName}`);
+      setWeatherData(response.data);
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+      setWeatherData(null);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter city name"
+          value={cityName}
+          onChange={(e) => setCityName(e.target.value)}
+        />
+        <button onClick={handleGetWeather}>Get Weather</button>
+      </div>
+      {weatherData && (
         <div>
-            <div>ここに処理を書いていきます</div>
-            {data ? <div>{data.Hello}</div> : <button onClick={GetData}>データを取得</button>}
+          <h2>Weather in {weatherData.city}</h2>
+          <p>Description: {weatherData.weather}</p>
+          <p>Temperature: {weatherData.temperature} °C</p>
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default App;
