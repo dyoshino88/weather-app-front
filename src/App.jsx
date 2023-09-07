7667ee69b6b9313cbf931faf62192bc23789275d
dyoshino88 <yoshino0707dh@gmail.com>
2023-09-07 17:19:10 +0900

    prefecture add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,60 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
   const [cityName, setCityName] = useState('');
   const [weatherData, setWeatherData] = useState(null);
+
+  // 日本の47都道府県のリストを作成
+  const prefectures = [
+    'Hokkaido',
+    'Aomori',
+    'Iwate',
+    'Miyagi',
+    'Akita',
+    'Yamagata',
+    'Fukushima',
+    'Ibaraki',
+    'Tochigi',
+    'Gunma',
+    'Saitama',
+    'Chiba',
+    'Tokyo',
+    'Kanagawa',
+    'Niigata',
+    'Toyama',
+    'Ishikawa',
+    'Fukui',
+    'Yamanashi',
+    'Nagano',
+    'Gifu',
+    'Shizuoka',
+    'Aichi',
+    'Mie',
+    'Shiga',
+    'Kyoto',
+    'Osaka',
+    'Hyogo',
+    'Nara',
+    'Wakayama',
+    'Tottori',
+    'Shimane',
+    'Okayama',
+    'Hiroshima',
+    'Yamaguchi',
+    'Tokushima',
+    'Kagawa',
+    'Ehime',
+    'Kochi',
+    'Fukuoka',
+    'Saga',
+    'Nagasaki',
+    'Kumamoto',
+    'Oita',
+    'Miyazaki',
+    'Kagoshima',
+    'Okinawa'
+  ];
 
   const handleGetWeather = async () => {
     try {
@@ -17,21 +68,26 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <h1>天気アプリ</h1>
       <div>
-        <input
-          type="text"
-          placeholder="Enter city name"
+        <select
           value={cityName}
           onChange={(e) => setCityName(e.target.value)}
-        />
-        <button onClick={handleGetWeather}>Get Weather</button>
+        >
+          <option value="">都市を選択してください</option>
+          {prefectures.map((prefecture) => (
+            <option key={prefecture} value={prefecture}>
+              {prefecture}
+            </option>
+          ))}
+        </select>
+        <button onClick={handleGetWeather}>今日の天気を取得</button>
       </div>
       {weatherData && (
         <div>
-          <h2>Weather in {weatherData.city}</h2>
-          <p>Description: {weatherData.weather}</p>
-          <p>Temperature: {weatherData.temperature} °C</p>
+          <h2>{weatherData.city}の予報</h2>
+          <p>天気: {weatherData.weather}</p>
+          <p>温度: {weatherData.temperature} °C</p>
         </div>
       )}
     </div>
